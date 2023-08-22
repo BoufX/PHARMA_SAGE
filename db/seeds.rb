54bf681a7b3dd74378b6d5c289d2543e63c7c783
oumaima bikra <bikraoumaima8@gmail.com>
2023-08-22 18:55:33 +0200

    adding order seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,13 +10,13 @@ User.destroy_all
 Pharmacy.destroy_all
 Order.destroy_all
 
-user = User.create!(email:"bob@gmail.com", password:"123456")
-Pharmacy.destroy_all
-pharmacy = Pharmacy.create(name: "Le Wagon Pharmacy", address: "139, Rue Abou Ishak El Marouni", phone_number: "0612234545")
+User.create!(email:"bob@gmail.com", password:"123456")
+Pharmacy.create(name: "Le Wagon Pharmacy", address: "139, Rue Abou Ishak El Marouni", phone_number: "0612234545")
+
+=begin
 Pharmacy.create(name: "Bouf's Pharmacy", address: "285, Bd Ghandi", phone_number: "06217459867")
 Pharmacy.create(name: "La Paix Pharmacy", address: "38 Bd Al Massira Al Khadra", phone_number: "0622147844")
 Pharmacy.create(name: "Helmon's Pharmacy", address: "47 Bd Hay Hassani", phone_number:"0655879654")
+=end
 
-Order.create!(user_id: user, pharmacy_id: pharmacy, name: 'DOLIPRNE 1000mg', price: '25')
-Order.create!(name: 'NOVICLIN 100 mg', price: '63')
-Order.create!(name: 'UNILUX 370 mg', price: '598')
+Order.create!(user_id: User.first.id, pharmacy_id: Pharmacy.first.id, name: 'DOLIPRNE 1000mg', price: '25')

4e72fc69f6888f66ba6083f9ce98076a23389ed5
abdallah boufrad <abdallahboufrad@gmail.com>
2023-08-24 16:05:54 +0100

    Pharmacy list changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,15 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+Order.destroy_all
 User.destroy_all
 Pharmacy.destroy_all
-Order.destroy_all
 
 User.create!(email:"bob@gmail.com", password:"123456")
-Pharmacy.create(name: "Le Wagon Pharmacy", address: "139, Rue Abou Ishak El Marouni", phone_number: "0612234545")
+Pharmacy.create(name: "Le Wagon Pharmacy", address: "139, Rue Ishak El Marouni", phone_number: "+212-612-23-45-45")
+Pharmacy.create(name: "Abdallah's Pharmacy", address: "285, Bd Ghandi El Idrissi", phone_number: "+212-621-74-59-86")
+Pharmacy.create(name: "Oussama's Pharmacy", address: "38 Bd Al Massira Al Khadra", phone_number: "+212-622-14-78-44")
+Pharmacy.create(name: "Oumaima's Pharmacy", address: "47 Bd Hay Hassani Chergui", phone_number:"+212-655-87-96-54")
 
-=begin
-Pharmacy.create(name: "Abdallah's Pharmacy", address: "285, Bd Ghandi", phone_number: "06217459867")
-Pharmacy.create(name: "Oussama's Pharmacy", address: "38 Bd Al Massira Al Khadra", phone_number: "0622147844")
-Pharmacy.create(name: "Oumaima's Pharmacy", address: "47 Bd Hay Hassani", phone_number:"0655879654")
-=end
 
 Order.create!(user_id: User.first.id, pharmacy_id: Pharmacy.first.id, name: 'DOLIPRNE 1000mg', price: '25')

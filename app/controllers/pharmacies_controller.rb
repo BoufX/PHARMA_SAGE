82301c16f3b80aef755813d792143b45e2ba22f5
abdallah boufrad <abdallahboufrad@gmail.com>
2023-08-25 10:29:48 +0100

    map changes

--- app/controllers/pharmacies_controller.rb
+++ app/controllers/pharmacies_controller.rb
@@ -4,11 +4,19 @@ class PharmaciesController < ApplicationController
  def index 
     @pharmacies = Pharmacy.all
     #@pharmacies = Pharmacy.where("")
+    @markers = @pharmacies.geocoded.map do |pharmacy|
+      {
+        lat: pharmacy.latitude,
+        lng: pharmacy.longitude
+      }
+    end
  end
 
  def show
     @pharmacy = Pharmacy.find(params[:id])
  end
+
+
 
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_22_203016) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_24_155249) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -46,8 +46,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_22_203016) do
   end
 
   create_table "pharmacies", force: :cascade do |t|
+    t.string "name"
+    t.string "phone_number"
+    t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "users", force: :cascade do |t|

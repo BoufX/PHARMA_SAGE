236f9055f0a9790f512d233f1d70ab8a34dcb549
Oussama El Boukioty <oelboukioty@gmail.com>
2023-08-25 16:53:40 +0100

    home page

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+require("@rails/ujs").start()
+require("@rails/activestorage").start()
+require("channels")
+
+// add these 2 lines
+import 'src/stylesheets/application'
+const images = require.context('../images', true)
